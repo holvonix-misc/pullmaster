96cb743639ef5e3f0f8ed594c679db5faf79dbaf
Advay Mengle (at Holvonix LLC) <advay-github@holvonix.com>
2017-12-10 19:08:57 -0800

    Finally got that regex right?

--- setup/setup-new-project.sh
+++ setup/setup-new-project.sh
@@ -25,7 +25,7 @@ sed -i -- "s/new-ball/$NEW_PROJECT_NAME/g" `find src/* -maxdepth 1 -type f`
 sed -i -- "s/new-ball/$NEW_PROJECT_NAME/g" setup/SETUP.md
 REVHEAD=`git rev-parse HEAD`
 sed -i -- "s/NEWBALLHEADREF/$REVHEAD/g" setup/SETUP.md
-sed -i -- "s/newball/$NEW_PROJECT_NAME/g" setup/SETUP.md
+sed -i -- "s/newball/new-ball/g" setup/SETUP.md
 
 # Silly sed files.
 rm -f .*--
