fe33bb80851a63c7c69b04e360b15def06f56461
Advay Mengle (at Holvonix LLC) <advay-github@holvonix.com>
2017-12-12 02:14:02 -0800

    Remove all existing Codacy issues

--- setup/setup-new-project.sh
+++ setup/setup-new-project.sh
@@ -19,11 +19,11 @@ git config --add commit.gpgsign true
 
 rm -f yarn.lock
 
-sed -i -- "s/new-ball/$NEW_PROJECT_NAME/g" `find . -maxdepth 1 -type f`
-sed -i -- "s/new-ball/$NEW_PROJECT_NAME/g" `find scripts/* -maxdepth 1 -type f`
-sed -i -- "s/new-ball/$NEW_PROJECT_NAME/g" `find src/* -maxdepth 1 -type f`
+sed -i -- "s/new-ball/$NEW_PROJECT_NAME/g" $(find . -maxdepth 1 -type f)
+sed -i -- "s/new-ball/$NEW_PROJECT_NAME/g" $(find scripts/* -maxdepth 1 -type f)
+sed -i -- "s/new-ball/$NEW_PROJECT_NAME/g" $(find src/* -maxdepth 1 -type f)
 sed -i -- "s/new-ball/$NEW_PROJECT_NAME/g" setup/SETUP.md
-REVHEAD=`git rev-parse HEAD`
+REVHEAD=$(git rev-parse HEAD)
 sed -i -- "s/NEWBALLHEADREF/$REVHEAD/g" setup/SETUP.md
 sed -i -- "s/newball/new-ball/g" setup/SETUP.md
 
