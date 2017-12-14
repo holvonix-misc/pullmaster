3d6426feda5268de3e3b019e9fbd97a530062d79
Advay Mengle (at Holvonix LLC) <advay-github@holvonix.com>
2017-12-14 06:38:56 -0800

    v0.3.0-alpha.4

--- src/index.js
+++ src/index.js
@@ -260,7 +260,7 @@ function handleDeferShipIt(
       const pr = z[0];
       const meta = z[1];
       const u: string = pr.base.repo.commits_url;
-      const post = u.replace("{/sha}", pr.head.sha) + "/comments";
+      const post = u.replace("{/sha}", "/" + pr.head.sha) + "/comments";
       const stringy = JSON.stringify(meta);
       return makeRequest(settings, post, {
         body: {
@@ -377,7 +377,8 @@ function getPullRequests(settings: any, repo: any, page: ?number) {
       pr.requested_reviewers = pr.requested_reviewers.filter(reviewer =>
         settings.reviewers.includes(reviewer.login)
       );
-    }); // If more pages exists, recursively retrieve the next page
+    });
+    // If more pages exists, recursively retrieve the next page
     if (pullRequests.length === PAGE_SIZE) {
       return getPullRequests(settings, repo, page + 1).then(_pullRequests =>
         pullRequests.concat(_pullRequests)
@@ -425,7 +426,8 @@ function getPullRequests(settings: any, repo: any, page: ?number) {
     // These are awaiting the reviewer's initial review
     pr.requested_reviewers.forEach(reviewer => {
       reviewers[reviewer.login]++;
-    }); // For these the reviewer has requested changes, and has yet to approve the // pull request
+    });
+    // For these the reviewer has requested changes, and has yet to approve the // pull request
     pr.reviews.forEach(review => {
       reviewers[review.user.login]++;
     });
